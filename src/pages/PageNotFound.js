03d1579c6e40dedd64d0c1e868d6133d191b8f64
Ping Guan <ping.guan@outlook.com>
2022-02-26 01:17:54 -0700

    login

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,11 +3,21 @@ import {Link} from "react-router-dom";
 function PageNotFound (props) {
     return(
         <>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">LoginPage</Link>
+
+                </li>
+                <li>
+                    <Link to="/dashboard">DashBoard Page</Link>
+                </li>
+            </ul>
+        </nav>
         
-        
-            <header>
-                <h1>Page Not Found</h1>
-            </header>
+        <header>
+            <h1>Page Not Found</h1>
+        </header>
         </>
     )
 }

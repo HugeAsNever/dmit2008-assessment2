27292c355235e9d17aedea6619a69f467cf112f0
Ping Guan <ping.guan@outlook.com>
2022-02-26 15:30:09 -0700

    login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link, useNavigate} from "react-router-dom";
-import {Form, Button} from "react-bootstrap";
+import {Form, Button, Container, Row, Col} from "react-bootstrap";
+import logo from './../logo.png';
 
  //add event listner
  //elm.addEventListener
@@ -18,40 +19,53 @@ function onHandleSubmit(evt) {
 
 return( 
     
-    <>
+    <Container>
         <nav>
             <ul>
                 <li><Link to="/">LoginPage</Link></li>
                 <li><Link to="/dashboard">DashBoard Page</Link></li>
             </ul>
         </nav>
+        <Row className='shadow-1-strong p-5 ' style={ {backgroundColor: "#E8EAF6"} }>
+            <Col className='p-5 border border-light ' style={ {backgroundColor: "#EEEEEE"} } >
+                <Row className='justify-content-md-center'>
+                    <Col md="auto"><h1 className='my-4'>Sign In to DashBoard</h1></Col>
+                    
+                </Row>
+                
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={onHandleSubmit}>
-                Submit
-            </Button>
-        </Form>
-
-        <heder>
-            <h1>Login Page</h1>
-        </heder>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Remember me" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={onHandleSubmit}>
+                        Login!
+                    </Button>
+                </Form>
+                <Row className='my-4'>
+                    <Col><Link className='link-info' to="/#">Forgot Password?</Link></Col>
+                    <Col>New user? <Link className='link-info' to="/#"> Sing up!</Link></Col>
+                </Row>
+                
+            </Col>
+            <Col className='p-5 border border-light' style={ {backgroundColor: "#E3F2FD"} }>
+                <img src={logo} className='img-fluid' alt='company logo' />
+            </Col>
+        </Row>
         
-    </>
+        
+    </Container>
 
 )
 

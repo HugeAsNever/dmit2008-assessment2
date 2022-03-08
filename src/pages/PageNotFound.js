9855d243c64fac2fac25d563d7e76f61843c6c3e
Ping Guan <ping.guan@outlook.com>
2022-03-08 10:19:51 -0700

    404

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,23 +1,15 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Container, Row, Col} from "react-bootstrap";
+import {AppBar} from './../components/appbar';
 function PageNotFound (props) {
     return(
         <>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">LoginPage</Link>
-
-                </li>
-                <li>
-                    <Link to="/dashboard">DashBoard Page</Link>
-                </li>
-            </ul>
-        </nav>
-        
-        <header>
-            <h1>Page Not Found</h1>
-        </header>
+            <AppBar/>
+            <Container>
+                <Row className='justify-content-md-center'>
+                    <Col className="py-5" md="auto" style={ {fontSize: "4rem"} }><h1>404: Page Not Found</h1></Col>
+                </Row>
+            </Container>
         </>
     )
 }
